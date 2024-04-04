b9b5bb4b0aa7b788cde47cc5c4787ed8b2d33218
Kimyokhon <nkimyoxon@gmail.com>
2024-04-04 12:17:10 +0500

    commit

---
+++ src/main/java/com/example/worldcamp/WorldCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.worldcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldCampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/worldcamp/WorldCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worldcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
